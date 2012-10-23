28043d531a58bf12764f5f2889aabcfd48e9ea43
ckchaudhary <ckchaudhary@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-23 10:38:51 +0000

    not required
    git-svn-id: https://plugins.svn.wordpress.org/buddypress-message-attachment/tags/1.1@616090 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bp-message-attachement/includes/filters.php
+++
@@ -1,8 +0,0 @@
-<?php
- 
- add_filter( 'bp_msgat_data_before_save', 'wp_filter_kses', 1 );
-
- add_filter( 'bp_msgat_data_message_id_before_save', 'wp_filter_kses', 1 );
- add_filter( 'bp_msgat_data_attachement_url_before_save', 'wp_filter_kses', 1 );
-
-?>
